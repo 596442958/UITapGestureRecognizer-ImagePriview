fe20089a30f0611f2f23eb3bf993f4ff1d1e1546
Miles <596442958@qq.com>
2016-02-11 13:52:58 +0800

    Initial Commit

---
+++ 手势/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  手势
+//
+//  Created by Miles on 2/11/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 手势/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  手势
+//
+//  Created by Miles on 2/11/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
